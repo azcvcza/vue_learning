267ba53567b27dc0fc190eb6a2adfaf9030efb71
韦粤信 <283940802@qq.com>
2018-12-01 17:38:13 +0800

    feature: template complete login

--- project-5-SupportCenter/src/components/NavMenu.vue
+++ project-5-SupportCenter/src/components/NavMenu.vue
@@ -1,12 +1,20 @@
 <template>
-	<nav class="menu">
-		<router-link :to="{name:'home'}" exact>Home</router-link>
-		<router-link :to="{name:'faq'}">FAQ</router-link>
-	</nav>
+  <nav class="menu">
+    <router-link :to="{name:'home'}" exact>Home</router-link>
+    <router-link :to="{name:'faq'}">FAQ</router-link>
+    <div class="spacer">
+      <template v-if="$state.user">
+        <a href>{{$state.user.username}}</a>
+        <a href @click="logout">Logout</a>
+      </template>
+	  <router-link v-else :to"{name:'login'}"></router-link>
+    </div>
+  </nav>
 </template>
 <style lang="stylus" scoped>
 @import '../style/imports';
-.router-link-active{
-	border-bottom-color:$primary-color;
+
+.router-link-active {
+  border-bottom-color: $primary-color;
 }
 </style>
--- project-5-SupportCenter/src/plugins/fetch.js
+++ project-5-SupportCenter/src/plugins/fetch.js
@@ -1,14 +1,34 @@
 let baseUrl;
-export async function $fetch(url) {
-    const response = await fetch(`${baseUrl}${url}`);
+export async function $fetch(url, options) {
+    const finalOptions = Object.assign({}, {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        credenital: 'include',
+    }, options);
+    const response = await fetch(`${baseUrl}${url}`, finalOptions);
+
     if (response.ok) {
         const data = await response.json();
         console.log("fetch.js,data: ", data);
         return data;
     } else {
-        const error = new Error('error');
+        const message = await response.text();
+        const error = new Error(message);
+        error.response = response;
         throw error;
     }
+}
+async function signup() {
+    await this.$fetch('signup', {
+        method: 'POST',
+        body: JSON.stringify({
+            username: this.username,
+            password: this.password,
+            email: this.email,
+        })
+    })
+    this.mode = 'login'
 }
 export default {
     install(Vue, options) {
